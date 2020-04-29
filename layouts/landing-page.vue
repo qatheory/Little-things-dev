d8f7ae543d8927bdd4674921e55ea7c0c2fef98c
quocanh <qatheorys@gmail.com>
2020-04-29 15:54:39 +0700

    inital

---
+++ layouts/landing-page.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      app
+      color="pink lighten-5"
+      flat
+      prominent
+      extended
+      src="/img/toobar_img.jpg"
+      absolute
+      extension-height="256"
+    >
+      <v-btn text>
+        Little things
+      </v-btn>
+      <v-btn text>
+        Bộ sưu tập
+      </v-btn>
+      <v-btn text>
+        Cửa hàng
+      </v-btn>
+      <v-spacer />
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>Tab 1</v-tab>
+          <v-tab>Tab 2</v-tab>
+          <v-tab>Tab 3</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+    <v-content>
+      <nuxt />
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
---
+++ pages/home.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-container fluid class="pa-0">
+    <v-row class="ma-0" max-height="100vh">
+      <v-col class="pa-0" lg="3" md="4" sm="6" xs="12">
+        <v-card data-aos="fade-right" data-aos-duration="800" height="100%">
+          <v-img height="100%" src="/img/cosmetic/img_side.jpg">
+            <v-row class=" white--text" style="margin-top:25% !important">
+              <v-col>
+                <div class="mx-auto" style="width:50% !important">
+                  <div class="display-2 font-italic">ĐÂY !!!</div>
+                  <div class="body-1 font-weight-thin	">Là loại mỹ phẩm</div>
+                  <div class="caption font-italic font-weight-light">
+                    Mà quăn đíu biết tên
+                  </div>
+                </div>
+              </v-col>
+            </v-row>
+          </v-img>
+        </v-card>
+      </v-col>
+      <v-col height="80%" class="pa-0" lg="9" md="8" sm="6" xs="12">
+        <v-row class="pa-0 ma-0" max-height="50vh">
+          <v-col class="pa-0" lg="8" md="8" sm="12" xs="12">
+            <v-card
+              data-aos="fade-up"
+              data-aos-duration="800"
+              data-aos-delay="100"
+              height="100%"
+            >
+              <v-img height="100%" src="/img/cosmetic/img_wide1.jpg">
+                <v-row
+                  class=" white--text"
+                  style="margin:5% 0 0 25% !important"
+                >
+                  <v-col>
+                    <div class="mx-auto" style="width:50% !important">
+                      <div class="body-1 font-weight-thin	">
+                        Cái này quăn cũng hăm bít
+                      </div>
+                      <div class="caption font-italic font-weight-light">
+                        nhưng chắc là kẻ mắt
+                      </div>
+                    </div>
+                  </v-col>
+                </v-row>
+              </v-img>
+            </v-card>
+          </v-col>
+          <v-col class="pa-0" lg="4" md="4" sm="12" xs="12">
+            <v-card
+              data-aos="fade-up"
+              data-aos-duration="800"
+              data-aos-delay="200"
+              height="100%"
+            >
+              <v-img height="100%" src="/img/cosmetic/img_sqrt1.jpg"> </v-img>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row class="pa-0 ma-0" max-height="50vh">
+          <v-col class="pa-0" lg="4" md="4" sm="12" xs="12">
+            <v-card data-aos="fade-up" data-aos-delay="50" height="100%">
+              <v-img height="100%" src="/img/cosmetic/img_sqrt2.jpg"> </v-img>
+            </v-card>
+          </v-col>
+          <v-col class="pa-0" lg="8" md="8" sm="12" xs="12">
+            <v-card data-aos="fade-up" data-aos-delay="150" height="100%">
+              <v-img height="100%" src="/img/cosmetic/img_wide2.jpg"> </v-img>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  layout: 'landing-page'
+}
+</script>
