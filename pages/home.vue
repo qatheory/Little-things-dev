43be539bc6e57a22a9525816cf5ee1c6c7642b0d
quocanh <qatheorys@gmail.com>
2020-04-29 17:24:31 +0700

    update link

--- pages/home.vue
+++ pages/home.vue
@@ -3,7 +3,7 @@
     <v-row class="ma-0" max-height="100vh">
       <v-col class="pa-0" lg="3" md="4" sm="6" xs="12">
         <v-card data-aos="fade-right" data-aos-duration="800" height="100%">
-          <v-img height="100%" src="/img/cosmetic/img_side.jpg">
+          <v-img height="100%" src="/img/cosmetics/img_side.jpg">
             <v-row class=" white--text" style="margin-top:25% !important">
               <v-col>
                 <div class="mx-auto" style="width:50% !important">
@@ -27,7 +27,7 @@
               data-aos-delay="100"
               height="100%"
             >
-              <v-img height="100%" src="/img/cosmetic/img_wide1.jpg">
+              <v-img height="100%" src="/img/cosmetics/img_wide1.jpg">
                 <v-row
                   class=" white--text"
                   style="margin:5% 0 0 25% !important"
@@ -53,19 +53,19 @@
               data-aos-delay="200"
               height="100%"
             >
-              <v-img height="100%" src="/img/cosmetic/img_sqrt1.jpg"> </v-img>
+              <v-img height="100%" src="/img/cosmetics/img_sqrt1.jpg"> </v-img>
             </v-card>
           </v-col>
         </v-row>
         <v-row class="pa-0 ma-0" max-height="50vh">
           <v-col class="pa-0" lg="4" md="4" sm="12" xs="12">
             <v-card data-aos="fade-up" data-aos-delay="50" height="100%">
-              <v-img height="100%" src="/img/cosmetic/img_sqrt2.jpg"> </v-img>
+              <v-img height="100%" src="/img/cosmetics/img_sqrt2.jpg"> </v-img>
             </v-card>
           </v-col>
           <v-col class="pa-0" lg="8" md="8" sm="12" xs="12">
             <v-card data-aos="fade-up" data-aos-delay="150" height="100%">
-              <v-img height="100%" src="/img/cosmetic/img_wide2.jpg"> </v-img>
+              <v-img height="100%" src="/img/cosmetics/img_wide2.jpg"> </v-img>
             </v-card>
           </v-col>
         </v-row>
